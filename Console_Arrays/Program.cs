2ad0ec98111ffc83bcc417ff18f8b44da93905fe
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-06 20:19:13 -0400

    1D Array Declare - Assign - Surfe

--- Console_Arrays/Program.cs
+++ Console_Arrays/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Console_Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //--------------- single-dimensional array
+            int[] myarray1 = new int[3];   //First Sturcture
+            myarray1[0] = 10;
+            myarray1[1] = 20;
+            myarray1[2] = 30;
+            //
+            int[] myarray2 = new int[] { 1, 2, 3 }; //Second Structure
+
+
+            //---------- Surfing 1D Array -- FOR
+            Console.WriteLine("Surfing 1D Array by For-Loop");
+            for (int i=0;i<3;i++)
+            {
+                Console.WriteLine(myarray1[i]);
+            }
+
+
+            //---------- Surfing 1D Array -- Foreach
+            Console.WriteLine("Surfing 1D Array by ForEach-Loop");
+            foreach (var i in myarray1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
         }
     }
 }
